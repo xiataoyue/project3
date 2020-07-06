a23fe4b48caa39fb9fdd1f06f72f5922aa545a65
xiataoyue <xiataoyue123456@outlook.com>
2020-07-06 13:33:53 +0800

    update

---
+++ Project-Three-Related-Files/cmake-build-debug/CMakeFiles/Blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Blackjack.exe"
+  "Blackjack.exe.manifest"
+  "Blackjack.pdb"
+  "CMakeFiles/Blackjack.dir/blackjack.cpp.obj"
+  "CMakeFiles/Blackjack.dir/deck.cpp.obj"
+  "CMakeFiles/Blackjack.dir/hand.cpp.obj"
+  "CMakeFiles/Blackjack.dir/player.cpp.obj"
+  "libBlackjack.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
